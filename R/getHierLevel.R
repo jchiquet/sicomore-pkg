d569108d4ac2b753203100513bb7b9ced5bd39aa
Florent GUINOT <guinotflorent@gmail.com>
2020-02-14 10:08:05 +0100

    modif sicomore algorithm with stability selection (stabs package)

--- R/getHierLevel.R
+++ R/getHierLevel.R
@@ -1,43 +1,14 @@
-#' @title getHierLevel
-#' @description A function to select groups of variables which are good for predicting a given phenotype.
-#' The groups considered corresponds various cut levels in a user defined hierarchy.
-#' The selection is performed by various penalty-based regression methods.
-#'
-#' @param X input matrix
-#' @param y response variable
-#' @param hierarchy the results of a hierarchical clustering algorithm, typically \code{hclust}. Must be an "hclust" object
-#' @param selection a string for the method used for variable selection for each data set.
-#' Specify "sicomore" to use the method specifically developed for the package, "hcar" to use the method developped by \insertCite{park;textual}{SIComORe} or
-#' "mlgl" for the method of \insertCite{grimonprez;textual}{SIComORe}.
-#' @param compression a string (either "mean" or "SNP.dist"). Indicates how the groups of variables are compressed before variable selection at each level of the hierarchy.
-#' @param cut.levels a numeric vector, the level consider in the hierarchy. By default a sequence of 100 levels is used.
-#' @param choice a string (either "lambda.min" or "lambda.1se"). Indicates how the tuning parameter is chosen in the penalized regression approach
-#' @param depth.cut an integer specifying the depth of the search space for the variable selection part of the algorithm.
-#' This argument allows to increase the speed of the algorithm by restraining the search space without affecting too much the performance.
-#' A value between 3 and 6 is recommended, the smaller the faster.
-#' @param mc.cores an integer for the number of cores to use in the parallelization of the cross-validation and some other functions.
-#'
-#' @return a RC object with class 'sicomore-model', with methods \code{nGrp()}, \code{nVar()}, \code{getGrp()}, \code{getVar()}, \code{getCV()}, \code{getX.comp()}, \code{getCoef()} and with the following fields:
-#' \itemize{
-#'  \item{groups:}{a list with the selected groups of predictors}
-#'  \item{coefficients:}{a vector with the estimated coefficients (one per selected group)}
-#'  \item{X.comp:}{The compressed version of the original input matrix (as many columns as number of selected groups)}
-#'  \item{cv.error:}{for the best grouping, a data frame showing the cross-validation error used in the variable selection procedure}
-#'  \item{selection:}{the selection method used}
-#'  \item{compression:}{the compression method used}
-#' }
-#' @include utils.R
-#' @import glmnet MLGL
-#' @export
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertAllCited{}
-#'
-getHierLevel <- function(X, y, hierarchy, selection = c("sicomore", "hcar", "mlgl"),
-                         compression = "mean", depth.cut = 3,
+getHierLevel <- function(X,
+                         y,
+                         hierarchy,
+                         selection = c("rho-sicomore", "sicomore", "mlgl"),
+                         compression = "mean",
+                         depth.cut = 3,
                          cut.levels  = unique(round(10^seq(log10(ncol(X)), log10(2), len=100))),
                          choice=c("lambda.min", "lambda.1se"),
-                         mc.cores=NULL) {
+                         mc.cores=NULL,
+                         stab = FALSE,
+                         stab.param = NULL) {
 
   ## _________________________________________________________________
   ##
@@ -59,16 +30,16 @@ getHierLevel <- function(X, y, hierarchy, selection = c("sicomore", "hcar", "mlg
   ##
   ## GET THE BEST COMPRESSION LEVEL IN THE HIERARCHY
   .getHier <- switch(selection,
+                     "rho-sicomore" = getHierLevel.rhosicomore,
                      "sicomore" = getHierLevel.sicomore,
-                     "hcar" = getHierLevel.HCAR,
                      "mlgl" = getHierLevel.MLGL)
 
-  out <- .getHier(X, y, hierarchy, compression, cut.levels, choice, depth.cut, mc.cores)
+  out <- .getHier(X, y, hierarchy, compression, cut.levels, choice, depth.cut, mc.cores, stab, stab.param)
 
   if (length(out$groups) > 0) {
     grouping <- rep(1:length(out$groups), sapply(out$groups, length))
     X.comp   <- cbind(computeCompressedDataFrame(X[, unlist(out$groups)], grouping, compression))
-    out$coefficients <- setNames(out$coefficients, paste("group", 1:length(out$groups)))
+    if(!stab) out$coefficients <- setNames(out$coefficients, paste("group", 1:length(out$groups)))
     out$groups       <- setNames(out$groups, NULL)
   } else {
     grouping <- cutree(hierarchy, k=1+which.max(rev(diff(hierarchy$height))))
@@ -77,17 +48,19 @@ getHierLevel <- function(X, y, hierarchy, selection = c("sicomore", "hcar", "mlg
   }
   if (selection=="pval") {
     criterion <- out$pval.count
-    }
-  else {
-    criterion <-  out$cv
   }
-  return(new("sicomore-model",
-             coefficients = out$coefficients,
+
+  res <- new("sicomore-model",
              groups       = out$groups      ,
-             cv.error     = criterion      ,
              X.comp       = X.comp          ,
              compression  = compression     ,
-             selection    = selection       ))
+             selection    = selection       )
+  if(!stab){
+    res$coefficients = out$coefficients
+    res$cv.error = out$cv
+  }
+
+  return(res)
 }
 
 ## __________________________________________________________________
@@ -96,7 +69,7 @@ getHierLevel <- function(X, y, hierarchy, selection = c("sicomore", "hcar", "mlg
 ##
 ## explore the hierarchy to find the best level of compression (cut.levels are the best)
 ## The bottom of the hiearchy is discarded.... (too many variables :( )
-getHierLevel.sicomore <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores) {
+getHierLevel.rhosicomore <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores, stab, stab.param) {
 
   weights <- sqrt(1/abs(diff(hc.object$height)))
   cut.levels <- order(rev(c(max(weights),weights)))+1
@@ -114,57 +87,95 @@ getHierLevel.sicomore <- function(X, y, hc.object, compression, cut.levels, choi
 
   ## Adjusting the model
   if (!is.null(mc.cores)) doMC::registerDoMC(cores=mc.cores)
+  else mc.cores = 1
   penalty.factor <- rep(weights[cut.levels],cut.levels)[uniqueIndex]
-  cv <- glmnet::cv.glmnet(Xcomp, y, penalty.factor = penalty.factor, parallel=!is.null(mc.cores))
 
-  ## Extracting the best model
-  selected.groups <- predict(cv, s=choice, type="nonzero")[[1]]
-  groups <- Xcomp.variables[selected.groups]   # elements of each group of the best model
-  coefficients <- predict(cv, s=choice, type="coefficients")[-1] # without intercept
-  coefficients <- coefficients[selected.groups]
+  if (stab == TRUE){
+    stab.lasso <- stabs::stabsel(x = Xcomp, y = y, B = stab.param$B,
+                          cutoff = stab.param$cutoff, PFER = stab.param$PFER,
+                          fitfun = glmnet.lasso,
+                          args.fitfun = list(penalty.factor = penalty.factor),
+                          mc.cores = mc.cores, sampling.type = "MB")
+    selected.groups <- as.numeric(stab.lasso$selected)
+    groups <- Xcomp.variables[selected.groups]   # elements of each group of the best model
+    res <- list(groups = groups)
+  } else {
+    cv <- glmnet::cv.glmnet(Xcomp, y, penalty.factor = penalty.factor, parallel=!is.null(mc.cores))
+    selected.groups <- predict(cv, s=choice, type="nonzero")[[1]]
+    groups <- Xcomp.variables[selected.groups]   # elements of each group of the best model
+    coefficients <- predict(cv, s=choice, type="coefficients")[-1] # without intercept
+    coefficients <- coefficients[selected.groups]
+    res <- list(cv = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda),
+                groups = groups, coefficients = coefficients)
 
-  return(list(cv = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda),
-              groups = groups, coefficients = coefficients))
+  }
+
+  return(res)
 }
 
 ## __________________________________________________________________
 ##
 ## CROSS-VALIDATED COMPRESSED LASSO ALONG THE HIERARCHY
 ##
-## explore the hiearchy to find the best level of compression
-getHierLevel.HCAR <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores) {
+## explore the hierarchy to find the best level of compression
+getHierLevel.sicomore <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores, stab, stab.param) {
 
   ## recovering the hierarchies at each cuting level.
   hierarchy <- lapply(apply(cutree(hc.object, k = cut.levels), 2, list), unlist, recursive=FALSE)
 
-  ## recover the grid of penalties used for comparison
   if (!is.null(mc.cores)) doMC::registerDoMC(cores=mc.cores)
-  all.cv <- lapply(hierarchy, function(group) {
-    return(glmnet::cv.glmnet(computeCompressedDataFrame(X, group, compression), y, parallel=!is.null(mc.cores)))
-  })
+  else mc.cores = 1
 
-  ## piles up the cv.error of each level of the hierarchy for all lambda
-  cv.error <- do.call(rbind,
-                      lapply(all.cv, function(cv) {
-                        data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda, ngroup = cv$glmnet.fit$dim[1])
-                      }))
+  Xcomp.variables <- unlist(lapply(hierarchy,function(group) {lapply(1:max(group),function(k){which(group==k)})}), recursive=FALSE)
+  uniqueIndex <- !duplicated(Xcomp.variables)
+  Xcomp.variables <- Xcomp.variables[uniqueIndex] # Eliminate the group which are present at different level of the hierarchy
+  Xcomp <- do.call(cbind,lapply(Xcomp.variables, function(variables) {computeCompressedDataFrameFromVariables(X, variables, compression)}))
 
-  ## Extract the best model
-  ibest <- match(cv.error$ngroup[which.min(cv.error$mean)], cut.levels)
-  selected.group <- predict(all.cv[[ibest]], s=choice, type="nonzero")[[1]]
-  groups    <- lapply(selected.group, function(x) which(hierarchy[[ibest]] == x))
-  coefficients <- predict(all.cv[[ibest]], s=choice, type="coefficients")[-1,] # without intercept
-  coefficients <- coefficients[selected.group]
+  if (stab == TRUE){
+       stab.lasso <- stabs::stabsel(x = Xcomp, y = y, B = stab.param$B,
+                          cutoff = stab.param$cutoff, PFER = stab.param$PFER,
+                          fitfun = glmnet.lasso, mc.cores = mc.cores, sampling.type = "MB")
+    selected.groups <- as.numeric(stab.lasso$selected)
+    groups <- Xcomp.variables[selected.groups]   # elements of each group of the best model
+    res <- list(groups = groups)
 
-  return(list(cv = data.frame(mean = all.cv[[ibest]]$cvm, sd = all.cv[[ibest]]$cvsd, lambda  = all.cv[[ibest]]$lambda),
-              groups = groups, coefficients = coefficients))
+  } else {
+    cv <- glmnet::cv.glmnet(Xcomp, y, parallel=!is.null(mc.cores))
+    selected.groups <- predict(cv, s=choice, type="nonzero")[[1]]
+    groups <- Xcomp.variables[selected.groups]   # elements of each group of the best model
+    coefficients <- predict(cv, s=choice, type="coefficients")[-1] # without intercept
+    coefficients <- coefficients[selected.groups]
+    res <- list(cv = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda),
+                groups = groups, coefficients = coefficients)
+
+    # all.cv <- lapply(hierarchy, function(group) {
+    #   return(glmnet::cv.glmnet(computeCompressedDataFrame(X, group, compression), y, parallel=!is.null(mc.cores)))
+    # })
+    # ## piles up the cv.error of each level of the hierarchy for all lambda
+    # cv.error <- do.call(rbind,
+    #                     lapply(all.cv, function(cv) {
+    #                       data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda, ngroup = cv$glmnet.fit$dim[1])
+    #                     }))
+    #
+    # ## Extract the best model
+    # ibest <- match(cv.error$ngroup[which.min(cv.error$mean)], cut.levels)
+    # selected.group <- predict(all.cv[[ibest]], s=choice, type="nonzero")[[1]]
+    # groups    <- lapply(selected.group, function(x) which(hierarchy[[ibest]] == x))
+    # coefficients <- predict(all.cv[[ibest]], s=choice, type="coefficients")[-1,] # without intercept
+    # coefficients <- coefficients[selected.group]
+    #
+    # res <- list(cv = data.frame(mean = all.cv[[ibest]]$cvm, sd = all.cv[[ibest]]$cvsd, lambda  = all.cv[[ibest]]$lambda),
+    #             groups = groups, coefficients = coefficients)
+  }
+
+  return(res)
 }
 
 ## __________________________________________________________________
 ##
 ## CROSS-VALIDATED MULTILAYER GROUP-LASSO ALONG THE HIERARCHY
 ##
-getHierLevel.MLGL <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores) {
+getHierLevel.MLGL <- function(X, y, hc.object, compression, cut.levels, choice, depth.cut, mc.cores, stab, stab.param) {
 
   ## Grimponprez's weights
   weights <- c(0, sqrt(1/abs(diff(hc.object$height))), 0)
--- R/sicomore.R
+++ R/sicomore.R
@@ -1,54 +1,19 @@
-#' @title sicomore
-#' @description Selection of Interaction effects in COmpressed  Multiple Omics REpresentation
-#'
-#' From a set of input matrices and phenotype related to the same set of individual, sicomore is a two-step method which
-#' 1. find and select groups of correlated variables in each input matrix which are good predictors for the common phenotype
-#' 2. find the most predictive interaction effects between the set of data by testing for interaction between the selected groups of each input matrix
-#'
-#' @param y response variable (phenotype)
-#' @param X.list a list of input matrices. Must have the same number of rows (number of observations). May have different number of columns (predictors)
-#' @param compressions a vector of string for the compression methods for each data sets. Default used the mean to compressed predictors in the same group.
-#' @param method.clus a vector of string specifying the method used for the hierarchical clustering, one for each input matrix in X.list.
-#' By default, the hierarchy is obtain by a WARD clustering on the scaled input matrix.
-#' To use an SNP-specific spatially contrained hierarchical clustering \insertCite{dehman}{SIComORe} from package adjclust, specify "snpClust".
-#' It is also possible to specify no hierarchy with "noclust".
-#' @param selection a string for the method used for variable selection for each data set.
-#' Specify "sicomore" to use the method specifically developed for the package, "hcar" to use the method developped by \insertCite{park;textual}{SIComORe} or
-#' "mlgl" for the method of \insertCite{grimonprez;textual}{SIComORe}.
-#' @param cuts a list of numeric vector defining the cut levels to be considered for each hierarchy. By default a sequence of 100 levels is used.
-#' @param choice a string (either "lambda.min" or "lambda.1se"). Indicates how the tuning parameter is chosen in the penalized regression approach.
-#' @param depth.cut a vector of integers specifying the depth of the search space for the variable selection part of the algorithm.
-#' This argument allows to increase the speed of the algorithm by restraining the search space without affecting too much the performance.
-#' A value between 3 and 6 is recommended, the smaller the faster.
-#' @param taxonomy a hierarchical tree object constructed using taxonomical unit.
-#' This argument could be useful if one want to provid a specific hierarchical tree based on taxonomy rather than euclidean distance.
-#' Recommended for those who want to detect genomic-metagenomic interactions.
-#' @param mc.cores an integer for the number of cores to use in the parallelization of the cross-validation and some other functions.
-#'
-#' @return a RC object with class \code{sicomore} with methods \code{plot()}, \code{getSignificance()} and the following fields
-#' \itemize{
-#'  \item{pval:}{A matrix of p-values for each interactions effects between the compressed variables originating from 2 input matrices.}
-#'  \item{pval.beta1:}{A matrix of p-values for the corresponding main effects of the compressed variables originating from the first input matrix}
-#'  \item{pval.beta2:}{A matrix of p-values for the corresponding main effects of the compressed variables originating from thesecond input matrix}
-#'  \item{models}{A class'sicomore-model' RC object obtain from \code{getHierLevel} function and with methods \code{nGrp()}, \code{nVar()}, \code{getGrp()}, \code{getVar()},
-#'  \code{getCV()}, \code{getX.comp()}, \code{getCoef()}}
-#'  \item{tuplets:}{A list of integer vector specifying the indexes of the compressed variables which are fitted together in a linear model with interaction.}
-#'  \item{dim:} A array of integers specifying the dimension of the compressed matrices.
-#' }
-#' @export
-#' @importFrom Rdpack reprompt
-#' @references
-#' \insertAllCited{}
-#'
-sicomore <- function(y, X.list,
+sicomore <- function(y,
+                     X.list,
                      compressions = rep("mean", length(X.list)),
-                     selection    =  c("sicomore", "hcar", "mlgl"),
+                     selection =  c("rho-sicomore", "sicomore", "mlgl"),
                      cuts = lapply(X.list, function(X) unique(round(10^seq(log10(ncol(X)), log10(2), len=100)))),
-                     choice=c("lambda.min", "lambda.min"), method.clus = c("ward.D2","ward.D2"),
-                     depth.cut = c(3,3), mc.cores = NULL, taxonomy = NULL, verbose = TRUE)
+                     choice=c("lambda.min", "lambda.min"),
+                     method.clus = c("ward.D2","ward.D2"),
+                     depth.cut = c(3,3),
+                     mc.cores = NULL,
+                     taxonomy = NULL,
+                     verbose = TRUE,
+                     stab = TRUE,
+                     stab.param  = list(B = c(100,100), cutoff = c(.75,.75), PFER = c(1,1)))
 {
 
-  selection <- match.arg(selection, c("sicomore", "hcar", "mlgl"))
+  selection <- match.arg(selection, c("rho-sicomore", "sicomore", "mlgl"))
   ndata <- length(X.list)
   models <- vector("list", ndata)
   hierarchies  <- vector("list" , length(X.list))
@@ -57,8 +22,9 @@ sicomore <- function(y, X.list,
 
     if (method.clus[i] == "ward.D2"){
       hierarchies[[i]] <- hclust(dist(t(scale(X.list[[i]]))), method="ward.D2")
-      models[[i]] <- getHierLevel(X.list[[i]], y, hierarchies[[i]], cut.levels = cuts[[i]], compression=compressions[i],
-                                  selection=selection, choice=choice[i], depth.cut = depth.cut[i], mc.cores=mc.cores)
+      models[[i]] <- getHierLevel(X = X.list[[i]], y, hierarchies[[i]], cut.levels = cuts[[i]], compression=compressions[i],
+                                  selection=selection, choice=choice[i], depth.cut = depth.cut[i], mc.cores=mc.cores,
+                                  stab, stab.param = lapply(stab.param, function(x) x[[i]]))
     }
     if (method.clus[i] == "snpClust") {
       if (ncol(X.list[[i]]) > 600) h <- 600
@@ -69,12 +35,28 @@ sicomore <- function(y, X.list,
     }
     if (method.clus[i] == "noclust"){
       if (!is.null(mc.cores)) doMC::registerDoMC(cores=mc.cores)
-      cv <- cv.glmnet(X.list[[i]], y, parallel=!is.null(mc.cores))
-      groups <- predict(cv, s=choice[i], type="nonzero")[[1]]
-      coefficients <- predict(cv, s=choice[i], type="coefficients")[-1] # without intercept
-      coefficients <- coefficients[groups]
-      models[[i]] <- new("sicomore-model", coefficients = coefficients, groups = as.list(groups),
-                         X.comp = X.list[[i]][,groups], cv.error = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda))
+      else mc.cores = 1
+
+      if (stab == TRUE){
+        stab.lasso <- stabs::stabsel(x = X.list[[i]], y = y, B = 50,
+                              fitfun = glmnet.lasso, cutoff = 0.75,
+                              PFER = 1, mc.cores = mc.cores)
+        groups <- as.numeric(stab.lasso$selected)
+        models[[i]] <- new("sicomore-model",
+                           groups = as.list(groups),
+                           X.comp = X.list[[i]][,groups],
+                           probs = stab.lasso$max)
+      } else{
+        cv <- cv.glmnet(X.list[[i]], y, parallel=!is.null(mc.cores))
+        groups <- predict(cv, s=choice[i], type="nonzero")[[1]]
+        coefficients <- predict(cv, s=choice[i], type="coefficients")[-1] # without intercept
+        coefficients <- coefficients[groups]
+        models[[i]] <- new("sicomore-model",
+                           coefficients = coefficients,
+                           groups = as.list(groups),
+                           X.comp = X.list[[i]][,groups],
+                           cv.error = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda))
+      }
     }
     if (method.clus[i] == "taxonomy"){
       if (is.null(taxonomy)) stop("Please provide taxonomy for Metagenomic data", call. = F)
@@ -99,28 +81,46 @@ sicomore <- function(y, X.list,
       group.name <- do.call(c,group.name)
 
       if (!is.null(mc.cores)) doMC::registerDoMC(cores=mc.cores)
+      else mc.cores = 1
       sel <- NULL
       while (length(sel) == 0){
-        cv <- cv.glmnet(Xcomp, y, parallel=!is.null(mc.cores))
-        sel <- predict(cv, s=choice[i], type="nonzero")[[1]]
-        group.select <- group.name[sel]
-        sel <- sel[-which(duplicated(group.select))]  ## Remove duplicated species
-        group.select[which(duplicated(group.select))] <- NULL
+        if(stab == TRUE){
+          stab.lasso <- stabs::stabsel(x = Xcomp, y = y, B = 50,
+                                fitfun = glmnet.lasso, cutoff = 0.75,
+                                PFER = 1, mc.cores = mc.cores)
+          sel <- as.numeric(stab.lasso$selected)
+          group.select <- group.name[sel]
+          sel <- sel[-which(duplicated(group.select))]  ## Remove duplicated species
+          group.select[which(duplicated(group.select))] <- NULL
+          probs <- stab.lasso$max
+        } else {
+          cv <- cv.glmnet(Xcomp, y, parallel=!is.null(mc.cores))
+          sel <- predict(cv, s=choice[i], type="nonzero")[[1]]
+          group.select <- group.name[sel]
+          sel <- sel[-which(duplicated(group.select))]  ## Remove duplicated species
+          group.select[which(duplicated(group.select))] <- NULL
+          ## Get coefficients
+          coefficients <- predict(cv, s=choice[i], type="coefficients")[-1] # without intercept
+          coefficients <- coefficients[sel]
+
+          models[[i]] <- new("sicomore-model", coefficients = coefficients, groups = group.select, X.comp = X.comp,
+                             cv.error = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda))
+        }
       }
-      ## Get coefficients
-      coefficients <- predict(cv, s=choice[i], type="coefficients")[-1] # without intercept
-      coefficients <- coefficients[sel]
 
       ## If no main effets are selected, keep the single variables with no compression
       if (length(sel) == 0) {
         warning("All coeffs are null, single variables are kept as main effects", call.=F, immediate.=TRUE)
         X.comp <- X.list[[i]]
         group.select <- as.list(1:ncol(X.list[[i]]))
+        probs <- NULL
+        models[[i]] <- new("sicomore-model",
+                           groups = group.select,
+                           probs = probs)
       }
       else X.comp <-  Xcomp[,sel]
 
-      models[[i]] <- new("sicomore-model", coefficients = coefficients, groups = group.select, X.comp = X.comp,
-                         cv.error = data.frame(mean = cv$cvm, sd = cv$cvsd, lambda  = cv$lambda))
+      models[[i]]$X.comp <- X.comp
     }
   }
 
@@ -142,14 +142,15 @@ sicomore <- function(y, X.list,
   ## convert interaction pval to an array
   dims <- sapply(models, function(model) model$nGrp())
   pval.Array <- array(NA, dim = dims)
-  rownames(pval.Array) <- paste0("X1comp_",1:nrow(pval.Array))
-  colnames(pval.Array) <- paste0("X2comp_",1:ncol(pval.Array))
+  rownames(pval.Array) <- paste0("X1comp_", 1:nrow(pval.Array))
+  colnames(pval.Array) <- paste0("X2comp_", 1:ncol(pval.Array))
 
   pval.beta1 <- pval.Array ; pval.beta2 <- pval.Array ; pval.inter <- pval.Array
   pval.beta1[do.call(rbind, tuplets)] <- pval[1,]
   pval.beta2[do.call(rbind, tuplets)] <- pval[2,]
   pval.inter[do.call(rbind, tuplets)] <- pval[3,]
 
-  return(new("sicomore-fit", pval = pval.inter, pval.beta1 = pval.beta1, pval.beta2 = t(pval.beta2), tuplets = tuplets, models=models, dim=sapply(X.list, ncol)))
+  return(new("sicomore-fit", pval = pval.inter, pval.beta1 = pval.beta1, pval.beta2 = t(pval.beta2),
+             tuplets = tuplets, models=models, dim=sapply(X.list, ncol)))
 }
 
