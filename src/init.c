daef5fa0dbd89b79024eeb6d4faab7d0cd05b5c9
Julien Chiquet <julien.chiquet@inrae.fr>
2020-03-10 11:00:31 +0100

    registering C function

---
+++ src/init.c
@@ -0,0 +1,24 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP cWardHeaps(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP percDown(SEXP, SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"cWardHeaps", (DL_FUNC) &cWardHeaps, 12},
+    {"percDown",   (DL_FUNC) &percDown,    4},
+    {NULL, NULL, 0}
+};
+
+void R_init_SIComORe(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
